eb94dcc7de757510f0d1601b5ab14d50b4aa75a5
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-04-19 15:39:10 +0200

    Initial commit

---
+++ lib/decidim_metabase.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "decidim_metabase/version"
+
+module DecidimMetabase
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/decidim_metabase_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DecidimMetabase do
+  it "has a version number" do
+    expect(DecidimMetabase::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
